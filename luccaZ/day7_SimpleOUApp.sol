aa6d2eb2ff2b588500481018db71eba4107e2020
hikorido <hikorido@gmail.com>
2026-03-07 21:46:29 +0000

    fix typo in  day7 exercise

--- luccaZ/day7_SimpleOUApp.sol
+++ luccaZ/day7_SimpleOUApp.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contratct SimpleIOU {
+contract SimpleIOU {
   address public owner;
 
   //Track registered friends
@@ -105,7 +105,7 @@ contratct SimpleIOU {
     balances[msg.sender] -= _amount;
     (bool success, ) = payable(msg.sender).call{value: _amount}("");
     require(success, "Withdrawal failed.");
-    
+
     emit Transaction(msg.sender, payable(msg.sender), _amount, "Withdrawal");
   }
 
