8b75579ed6d597be62b45eecd42dfc1a42dc6bdf
ToomChuan <caiting0920@gmail.com>
2026-03-15 11:26:36 +0800

    Make transfer functions virtual in SimpleERC20

--- ToomChuan/Day12.{ERC20}.sol/Day12.{SimpleERC20}.sol
+++ ToomChuan/Day12.{ERC20}.sol/Day12.{SimpleERC20}.sol
@@ -19,7 +19,7 @@ contract SimpleERC20 {
         emit Transfer(address(0), msg.sender, totalSupply);// fromaddress is set as"0":铸造代币
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool) {
+    function transfer(address _to, uint256 _value) public virtual returns (bool) {
         require(balanceOf[msg.sender] >= _value, "Not enough balance");
         _transfer(msg.sender, _to, _value);
         return true;
@@ -32,7 +32,7 @@ contract SimpleERC20 {
         emit Transfer(_from, _to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _value) public virtual returns (bool) {
         require(balanceOf[_from] >= _value, "Not enough balance");
         require(allowance[_from][msg.sender] >= _value, "Allowance too low");
 
